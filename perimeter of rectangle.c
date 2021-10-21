526dd61f8111e5e9c38539e2ce86aeff43e90101
Ayush Srivastava <91680234+ayush0815@users.noreply.github.com>
2021-10-21 23:00:17 +0530

    Add files via upload

---
+++ perimeter of rectangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+float L,B,P;
+printf("Enter length of rectangle\n");
+scanf("%f",&L);
+printf("Enter breadth if rectangle\n");
+scanf("%f",&B);
+P=2.0*(L+B);
+printf("The perimeter of rectangle is \n %4.2f",P);
+
+    return 0;
+}
